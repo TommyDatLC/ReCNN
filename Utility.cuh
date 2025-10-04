3eb361571db0b88a89a03fccad725f5c8aef37db
TommyDatLC <datsieusaiya12@gmail.com>
2025-10-04 11:17:50 +0700

    First

---
+++ Utility.cuh
@@ -0,0 +1,19 @@
+//
+// Created by datdau on 10/4/25.
+//
+
+#ifndef RECNN_UTILITY_CUH
+#define RECNN_UTILITY_CUH
+
+
+class Utility {
+    __device__ void MatrixMul(float** matrix1,float** matrix2) {
+        for (int i  =0 ;i < n;i++) {
+
+        }
+    }
+
+};
+
+
+#endif //RECNN_UTILITY_CUH
