d8cec036b1c9557d0e189ede6077b46b488fb6c7
tommydatLCLaptop2 <datsieusaiya12@gmail.com>
2025-11-03 15:35:56 +0700

    Commit pour mon chef

---
+++ cmake-build-debug/CMakeFiles/ReCNN.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ReCNN.dir/cmake_device_link.o"
+  "CMakeFiles/ReCNN.dir/main.cu.o"
+  "CMakeFiles/ReCNN.dir/main.cu.o.d"
+  "ReCNN"
+  "ReCNN.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CUDA)
+  include(CMakeFiles/ReCNN.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/ReCNN.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for ReCNN.
+# This may be replaced when dependencies are built.
